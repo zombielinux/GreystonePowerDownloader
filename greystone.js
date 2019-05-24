c2cb7e730c95204b80ae94f3a37bf665827ec310
zombielinux <will@sutton-family.org>
2019-05-24 07:43:24 -0400

    Added get previous 7 days logic. Added date argument to get much older data in the past. Useful for bash scripting

--- greystone.js
+++ greystone.js
@@ -29,43 +29,49 @@ else {
 //	console.log(startDate)
 }
 
-got('https://secure.greystonepower.com/oscp/customizations/MDM/ExportCSV.aspx?IsNew=Y&id=Interval&mbrsep=' + secrets.memberNumber + '&stdate=' + startDate +'&interval=15&sliderval=0&meter=' + secrets.meterNumber + '&demandcode=00').then(response => { 
-	console.log(response.body)
-//	console.log(typeof response.body.split(/\r?\n/))
-	var entries = response.body.split(/\r?\n/)
-	var keys = entries[0].split(",")
-	for (i=0; i<keys.length; i++){
-		keys[i] = keys[i].trim()
-	}
-	for (i=1; i<entries.length-1; i++){
-		tempData = entries[i].split(",")
-		for (j=0; j< tempData.length; j++){
-			tempData[j] = tempData[j].trim()
+
+
+for (h=0; h<7; h++){
+	newStartDate = new Date(startDate.split("/")[2], startDate.split("/")[0], startDate.split("/")[1])
+	newStartDate.setDate(newStartDate.getDate()-h)
+	console.log(newStartDate)
+
+	got('https://secure.greystonepower.com/oscp/customizations/MDM/ExportCSV.aspx?IsNew=Y&id=Interval&mbrsep=' + secrets.memberNumber + '&stdate=' + newStartDate.getMonth()+"/"+newStartDate.getDate()+"/"+parseInt(newStartDate.getYear()+1900) +'&interval=15&sliderval=0&meter=' + secrets.meterNumber + '&demandcode=00').then(response => { 
+		console.log(response.body)
+	//	console.log(typeof response.body.split(/\r?\n/))
+		var entries = response.body.split(/\r?\n/)
+		var keys = entries[0].split(",")
+		for (i=0; i<keys.length; i++){
+			keys[i] = keys[i].trim()
 		}
-		date = tempData[2].split("/")
-		time = tempData[3].split(":")
-		minute = time[1].split(" ")
-		if (minute[1] == "PM" && time[0] < 12) {
-			time[0] = parseInt(time[0]) + 12
-		}		
-		var recordedTime = toTimestamp(tzOffset, date[2], date[0], date[1], time[0], minute[0])
+		for (i=1; i<entries.length-1; i++){
+			tempData = entries[i].split(",")
+			for (j=0; j< tempData.length; j++){
+				tempData[j] = tempData[j].trim()
+			}
+			date = tempData[2].split("/")
+			time = tempData[3].split(":")
+			minute = time[1].split(" ")
+			if (minute[1] == "PM" && time[0] < 12) {
+				time[0] = parseInt(time[0]) + 12
+			}		
+			var recordedTime = toTimestamp(tzOffset, date[2], date[0], date[1], time[0], minute[0])
+	                influx.writePoints([{
+	                        measurement: 'greystoneMeasured',
+	                        fields: {usage: tempData[4]},
+				timestamp: recordedTime,
+	                        tags: {account: secrets.memberNumber, unit: secrets.meterNumber , location: "McCready"}
+	                }], {
+	                        database: secrets.influxDatabase,
+	                        precision: 's',
+	                });
 
-                influx.writePoints([{
-                        measurement: 'greystoneMeasured',
-                        fields: {usage: tempData[4]},
-			timestamp: recordedTime,
-                        tags: {account: secrets.memberNumber, unit: secrets.meterNumber , location: "McCready"}
-                }], {
-                        database: secrets.influxDatabase,
-                        precision: 's',
-                });
 
 
 
-
-	} 
-})
-
+		} 
+	})
+}
 function toTimestamp(offset, year, month, day, hour, minute, second) {
 	if (!second) { second = "00"}
 //	console.log(year+"-"+month+"-"+day+"T"+hour+":"+minute+":"+second+"-"+("0"+offset).slice(-2)+":00")
